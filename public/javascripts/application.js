2b246fcbf25233f6eb3fb434d2199a2a2a0b4176
Diana Kimball <diana.kimball@gmail.com>
2011-04-24 23:26:33 -0700

    Improve copy, add footer

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,43 @@
-function resize_shadow(offset) {
+// function resize_shadow(offset) {
+//  var ratio = 1164/1735;
+//  var width = $('#banner').width();
+//  var height = width * ratio - (offset || 0);
+//  return $('#shadow').height(height);
+//}
+
+//$(function() {
+//  setTimeout(function() {
+//    resize_shadow(10).fadeIn(5000);
+//  }, 2000);
+//  $(window).resize(function() {
+//    resize_shadow();
+//  });
+// });
+
+function reposition_button() {
   var ratio = 1164/1735;
-  var width = $('#banner').width();
-  var height = width * ratio - (offset || 0);
-  return $('#shadow').height(height);
+  var horizontal-offset = $('#banner').width();
+  var height = width * ratio;
+  return $('#image_map').height(height);
 }
 
 $(function() {
-  setTimeout(function() {
-    resize_shadow(10).fadeIn(5000);
-  }, 2000);
+  reposition_button();
   $(window).resize(function() {
-    resize_shadow();
+    resize_image_map();
   });
+});
+
+$(function() {
+  setTimeout(function() {
+  $('#dark').fadeIn(5000);
+  }, 2000);
+});
+
+$(function() {
+$('#intrigue').click(function()
+    {
+     $('#contact').slideToggle(500, function() {
+    });
+});
 });
