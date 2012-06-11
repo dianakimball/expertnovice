587c1c6affbf9756bce99ef4b6eb62c06ce8fc9a
Diana Kimball <diana.kimball@gmail.com>
2012-06-10 21:45:13 -0400

    Add new post for dkc blog

--- expertnovice.rb
+++ expertnovice.rb
@@ -17,3 +17,16 @@ post '/form' do
   gibbon.list_subscribe(options)
   redirect 'http://blog.expertnovice.com'
 end
+
+post '/dk_form' do
+  require 'gibbon'
+  gibbon = Gibbon.new('e0fd1a1da8203273f0d7374de97d47a8-us2')
+  options = {
+    :id            => '7577694b51',
+    :email_address => "#{params[:email]}",
+    :double_optin  => false,
+    :send_welcome  => false,
+  }
+  gibbon.list_subscribe(options)
+  redirect 'http://blog.dianakimball.com'
+end
